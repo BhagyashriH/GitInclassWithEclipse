2ff6be56d25f4ad664b7d758fb390e39a324a795
BhagyashriH <bhagyashri.hedau91@gmail.com>
2023-03-03 18:02:25 +0530

    first commit

--- src/com/qa/Digital/DigitalMarketing.java
+++ src/com/qa/Digital/DigitalMarketing.java
@@ -3,8 +3,8 @@ package com.qa.Digital;
 public class DigitalMarketing {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
+		
+		System.out.println("Digital Marketing");
+		}
 
 }
